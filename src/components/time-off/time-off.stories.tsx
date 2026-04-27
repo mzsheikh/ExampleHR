5297811a96801109fc5835d9cd8b0651b12d5972
mzsheikh <zeeshansheikh2007@gmail.com>
2026-04-28 02:21:27 +0500

    Fixed storybook ESList error

--- src/components/time-off/time-off.stories.tsx
+++ src/components/time-off/time-off.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 import { expect, fn, userEvent, within } from "@storybook/test";
 import { EmployeeView } from "./employee-view";
 import { ManagerView } from "./manager-view";
